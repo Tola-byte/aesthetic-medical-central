fb8a79b53ff8c6290f58ecbea56d02a07449caa0
Wajud <wajudkareem757@gmail.com>
2024-12-25 07:14:18 +0100

    added product searching

--- src/app/(pages)/products/page.jsx
+++ src/app/(pages)/products/page.jsx
@@ -5,19 +5,20 @@ import Image from 'next/image';
 import { allProducts } from '@/app/components/productsList';
 import Checkboxes from '@/app/components/Checkboxes';
 import Sort from '@/app/components/Sort';
+import Search from '@/app/components/Search';
 
 const page = () => {
-  console.log(allProducts.length);
   const [products, setProducts] = useState([...allProducts]);
   return (
     <div className="pt-24 flex px-8">
-      <div className="w-1/5 ">
+      <div className="w-[15%]">
         <Checkboxes products={products} setProducts={setProducts} />
       </div>
 
       <div className="flex-1">
-        <div className="w-full flex justify-between items-start">
-          <h1 className="text-xl font-semibold flex-1">Explore our products</h1>
+        <div className="w-full flex justify-between">
+          <h1 className="text-xl font-semibold">Explore our products</h1>
+          <Search products={products} setProducts={setProducts} />
           <Sort setProducts={setProducts} products={products} />
         </div>
 
@@ -34,10 +35,10 @@ const page = () => {
               />
               <div className=" pt-2">
                 <h2 className="font-semibold">{product.name}</h2>
-                <p className="text-white bg-yellow-500 text-[10px] w-fit px-2 py-1 rounded-md mb-6">
+                <p className="text-white bg-yellow-500 text-[10px] w-fit px-2 py-1 rounded-md mt-1 mb-8">
                   #{product.price}
                 </p>
-                <button className="block w-24 mx-auto text-center text-sm bg-yellow-500 hover:bg-yellow-600 text-white py-1 rounded-sm">
+                <button className="block w-24 mx-auto text-center text-sm border border-yellow-500 text-yellow-500 hover:bg-yellow-50  py-1 rounded-sm">
                   Add to Cart
                 </button>
               </div>
--- src/app/components/Sort.jsx
+++ src/app/components/Sort.jsx
@@ -6,9 +6,10 @@ const Sort = ({ setProducts, products }) => {
   const handleChange = (event) => {
     const sortOption = JSON.parse(event.target.value);
     if (sortOption.field === 'label') {
-      if (sortOption.mode === 'AZ') {
-        const sortedProducts = products.sort((a, b) =>
-          a.name.localeCompare(b.name),
+      if (sortOption.mode === 'A to Z') {
+        const sortedProducts = products.sort(
+          (a, b) => a.name.localeCompare(b.name),
+          setSelectedOption(sortOption.mode),
         );
         setProducts([...sortedProducts]);
       } else {
@@ -16,27 +17,30 @@ const Sort = ({ setProducts, products }) => {
           b.name.localeCompare(a.name),
         );
         setProducts([...sortedProducts]);
+        setSelectedOption(sortOption.mode);
       }
     }
 
     if (sortOption.field === 'price') {
-      if (sortOption.mode === 'LH') {
+      if (sortOption.mode === 'Low to High') {
         const sortedProducts = products.sort((a, b) => a.price - b.price);
         setProducts([...sortedProducts]);
+        setSelectedOption(sortOption.mode);
       } else {
         const sortedProducts = products.sort((a, b) => b.price - a.price);
         setProducts([...sortedProducts]);
+        setSelectedOption(sortOption.mode);
       }
     }
   };
 
   return (
-    <div className="w-64 mx-auto ">
+    <div className="">
       <label
         htmlFor="styledSelect"
         className="block text-sm font-medium text-gray-700 mb-2"
       >
-        Sort
+        {/* Sort */}
       </label>
       <select
         id="styledSelect"
@@ -44,19 +48,19 @@ const Sort = ({ setProducts, products }) => {
         onChange={handleChange}
         className="block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 text-gray-700"
       >
-        <option value="" disabled>
-          Select an option
+        <option value="" className="py-2">
+          {selectedOption ? selectedOption : 'Sort'}
         </option>
-        <option value={JSON.stringify({ field: 'label', mode: 'AZ' })}>
+        <option value={JSON.stringify({ field: 'label', mode: 'A to Z' })}>
           A to Z
         </option>
-        <option value={JSON.stringify({ field: 'label', mode: 'ZA' })}>
+        <option value={JSON.stringify({ field: 'label', mode: 'Z to A' })}>
           Z to A
         </option>
-        <option value={JSON.stringify({ field: 'price', mode: 'LH' })}>
+        <option value={JSON.stringify({ field: 'price', mode: 'Low to High' })}>
           Low to High
         </option>
-        <option value={JSON.stringify({ field: 'price', mode: 'HL' })}>
+        <option value={JSON.stringify({ field: 'price', mode: 'High to Low' })}>
           High to Low
         </option>
       </select>
